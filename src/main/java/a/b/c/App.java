25f2225e9713440f3dff0685a284a2b4fe216e16
Ishan <ishananand930@gmail.com>
2026-02-14 21:41:46 +0530

    create operation

--- src/main/java/a/b/c/App.java
+++ src/main/java/a/b/c/App.java
@@ -9,20 +9,23 @@ public class App
 {
     public static void main( String[] args )
     {
-
-        Student so1=new Student();
-        so1.setS_id(2);
-        so1.setS_name("Ishita");
-        so1.setCity("Muzaffarpur");
+        // Student so1=new Student();
+        // so1.setS_id(2);
+        // so1.setS_name("Ishita");
+        // so1.setCity("Muzaffarpur");
         Configuration cfg= new Configuration();
         cfg.configure();
-
         SessionFactory sf=cfg.buildSessionFactory();
         Session s=sf.openSession();
         Transaction tr=s.beginTransaction();
 
+        Employee eo1=new Employee();
+        eo1.setEmp_fname("Ishita");
+        eo1.setEmp_lname("Anand");
+        eo1.setEmp_city("Muzaffarpur");
+
         try{
-            s.save(so1);
+            s.save(eo1);
             tr.commit();
         } catch(Exception e){
             tr.rollback();
---
+++ src/main/java/a/b/c/Employee.java
@@ -0,0 +1,37 @@
+package a.b.c;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private int emp_id;
+    private String emp_fname;
+    private String emp_lname;
+    private String emp_city;
+
+    public void setEmp_id(int emp_id){
+        this.emp_id=emp_id;
+    }
+    public int getEmp_id(){
+        return emp_id;
+    }
+    public void setEmp_fname(String emp_fname){
+        this.emp_fname=emp_fname;
+    }
+    public String getEmp_fname(){
+        return emp_fname;
+    }
+    public void setEmp_lname(String emp_lname){
+        this.emp_lname=emp_lname;
+    }
+    public String getEmp_lname(String emp_lname){
+        return emp_lname;
+    }
+    public void setEmp_city(String emp_city){
+        this.emp_city=emp_city;
+    }
+}
